5ee375f922dc7d901f237418d6e6697803dbbce7
K-Mozaffari <kamal_mozaffarie@yahoo.com>
2021-08-07 21:28:49 -0400

    Mealy Machin

--- Detector1010.vhd
+++ Detector1010.vhd
@@ -12,49 +12,70 @@ end entity;
 architecture bh of detector1010 is 
 
 type states is (idle,s1,s2,s3);
-signal state:states;
+signal Present_state,Next_state:states;
  
 begin 
+P_reg:    process (clk)
 
-process (clk)
+        begin 
+        if rising_edge(clk) then 
 
-begin 
+        Present_state<=Next_state;
+        end if;
 
-if rising_edge (clk) then 
-case state is 
+    end process;
 
-        when idle => if input ='0' then 
-                        state<=s1;
-                        output<='0';
-                    elsif input='1' then
-                        state<=idle;
-                        output<='0';
-                    end if;
-        when s1 => if input ='0' then 
-                        state<=s1;
-                        output<='0';
-                    elsif input='1' then
-                        state<=s2;
-                        output<='0';
-                    end if;
-        when s2 => if input ='0' then 
-                        state<=s3;
-                        output<='0';
-                    elsif input='1' then
-                        state<=idle;
-                        output<='0';
-                    end if;
-        when s3 => if input ='0' then 
-                        state<=s1;
-                        output<='0';
-                    elsif input='1' then
-                        state<=idle;
-                        output<='1';
-                    end if;
 
-end case;
-end if;
-end process;
+
+
+
+
+P_states:process (input,Present_state)
+
+        begin 
+                case Present_state is 
+
+                        when idle => if input ='0' then 
+                                        Next_state<=s1;
+                                    elsif input='1' then
+                                        Next_state<=idle;
+                                    end if;
+                        when s1 => if input ='0' then 
+                                        Next_state<=s1;
+                                    elsif input='1' then
+                                        Next_state<=s2;
+                                    end if;
+                        when s2 => if input ='0' then 
+                                        Next_state<=s3;
+                                    elsif input='1' then
+                                        Next_state<=idle;
+                                    end if;
+                        when s3 => if input ='0' then 
+                                        Next_state<=s1;
+                                    elsif input='1' then
+                                        Next_state<=idle;
+                                    end if;
+                end case;
+
+        end process;
+
+P_output: process (Present_state,input)
+        begin
+            case present_state is
+
+                    when idle => output<='0';
+                    when s1 => output<='0';
+                    when s2 => output<='0';
+                    when s3=>  if input ='0' then 
+				    
+				                 output<='0';
+				               elsif input='1' then 
+	                
+				                 output<='1';					
+				                end if;
+            end case;
+
+        end process;
 
 
 
