1dc547199c7bbce0069e89b9f7dd81e20d8d11da
K-Mozaffari <kamal_mozaffarie@yahoo.com>
2021-08-07 21:42:58 -0400

    Mealy Machin

--- Detector11011.vhd
+++ Detector11011.vhd
@@ -12,57 +12,70 @@ end entity;
 architecture bh of detector11011 is 
 
 type states is (idle,s1,s2,s3,s4);
-signal state:states;
+signal Present_state,Next_state:states;
  
 begin 
+P_reg: process(clk)
+        begin
+            if rising_edge(clk) then 
+                Present_state<=Next_state;
+            end if;
+        end process P_reg;
 
-process (clk)
 
-begin 
 
-if rising_edge (clk) then 
-case state is 
 
-        when idle => if input ='0' then 
-                        state<=idle;
-                        output<='0';
-                    elsif input='1' then
-                        state<=s1;
-                        output<='0';
-                    end if;
-        when s1 => if input ='0' then 
-                        state<=idle;
-                        output<='0';
-                    elsif input='1' then
-                        state<=s2;
-                        output<='0';
-                    end if;
-        when s2 => if input ='0' then 
-                        state<=s3;
-                        output<='0';
-                    elsif input='1' then
-                        state<=s2;
-                        output<='0';
-                    end if;
-        when s3 => if input ='0' then 
-                        state<=idle;
-                        output<='0';
-                    elsif input='1' then
-                        state<=s4;
-                        output<='0';
-                    end if;
-        when s4=> if input ='0' then 
-                        state<=idle;
-                        output<='0';
-                    elsif input='1' then
-                        state<=idle;
-                        output<='1';
-                    end if;
 
-end case;
-end if;
-end process;
+P_states: process (input,Present_state)
 
+            begin 
+             
+                case Present_state is 
+
+                        when idle => if input ='0' then 
+                                        Next_state<=idle;
+                                    elsif input='1' then
+                                        Next_state<=s1;
+                                    end if;
+                        when s1 => if input ='0' then 
+                                        Next_state<=idle;
+                                    elsif input='1' then
+                                        Next_state<=s2;
+                                    end if;
+                        when s2 => if input ='0' then 
+                                        Next_state<=s3;
+                                    elsif input='1' then
+                                        Next_state<=s2;
+                                    end if;
+                        when s3 => if input ='0' then 
+                                        Next_state<=idle;
+                                    elsif input='1' then
+                                        Next_state<=s4;
+                                    end if;
+                        when s4=> if input ='0' then 
+                                        Next_state<=idle;
+                                    elsif input='1' then
+                                        Next_state<=idle;
+                                    end if;
+                 
+               end case;
+            end process P_states;
+P_output: process (input,Present_state)
+                begin
+                    case Present_State is 
+                            when idle => output<='0';
+                            when s1   => output<='0';
+                            when s2   => output<='0';
+                            when s3   => output<='0';
+                            when s4   => if input='0' then
+                                            output<='0';
+                                         elsif input='1' then 
+                                            output<='1';
+                                        end if;
+                    end case;
+
+                        
+                end process P_output;
 
 
 
